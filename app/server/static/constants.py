81686e18ca99769e182d2f3092678df95c34100f
Chirag876 <chirag1706gupta@gmail.com>
2026-09-13 16:46:41 +0530

    changes : Removed files that are not required and removed the code which is not required

--- app/server/static/constants.py
+++ app/server/static/constants.py
@@ -1,5 +1,2 @@
 
 LOGGER_SERVICE_NAME = 'edulens-ai' # This name identifies the logs from this service in a microservice architecture, where logs from different services are centrally collected.
-
-OTP_EXPIRY_MINUTES = 1
-RESEND_OTP_AFTER_MIN = 1
